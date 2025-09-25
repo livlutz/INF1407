b27da8008707d094c5fd1948df5a6b8bc13ed188
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-09-25 11:50:57 +0000

    procedimento de esquecer a senha + alterar perfil

--- Aula_05/exemplo/exemplo/settings.py
+++ Aula_05/exemplo/exemplo/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#configuração do email para redefinição de senha
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'webmaster@localhost'
--- Aula_05/exemplo/exemplo/urls.py
+++ Aula_05/exemplo/exemplo/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from exemplo import views
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.models import User
 
 #separar as urls por aplicação -> colocando os links da aplicação
 #arquivo de rotas
@@ -43,5 +45,19 @@ urlpatterns = [
     path('seguranca/logout/', LogoutView.as_view(next_page=reverse_lazy('sec-home')), name='sec-logout'),
     path('seguranca/password_change/', PasswordChangeView.as_view(template_name='seguranca/password_change_form.html', success_url=reverse_lazy('sec-password-change-done')), name='sec-password-change'),
     path('seguranca/password_change/done/', PasswordChangeDoneView.as_view(template_name='seguranca/password_change_done.html'), name='sec-password-change-done'),
-
+    path('seguranca/editarPerfil/<int:pk>/', UpdateView.as_view(template_name='seguranca/user_form.html',
+                                                model=User,
+                                                fields=['first_name', 'last_name', 'email'],
+                                                success_url=reverse_lazy('sec-home')),
+         name='sec-editar-perfil'),
+    #links para resetar a senha
+    path('seguranca/password_reset/', PasswordResetView.as_view(
+                                                template_name='seguranca/password_reset_form.html',
+                                                success_url=reverse_lazy('sec-password-reset-done'),
+                                                subject_template_name='seguranca/password_reset_email.html',), name='sec-password-reset'),
+    path('seguranca/password_reset_done/', PasswordResetDoneView.as_view(template_name='seguranca/password_reset_done.html',), name='sec-password-reset-done'),
+    path('seguranca/password_reset_confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(
+                                                template_name='seguranca/password_reset_confirm.html',
+                                                success_url=reverse_lazy('sec-password-reset-complete'),), name='password_reset_confirm'),
+    path('seguranca/password_reset_complete/', PasswordResetCompleteView.as_view(template_name='seguranca/password_reset_complete.html'), name='sec-password-reset-complete'),
 ]
