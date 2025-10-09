f15aa7c2b2767c8e45cadae7ffd150923b46dcb4
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-10-09 11:45:55 +0000

    add initial TypeScript files and configuration

---
+++ Typescript/public/script/TiposDeVariaveis.js
@@ -0,0 +1,11 @@
+"use strict";
+//Novos tipos
+//Exemplos de constantes
+//Constantes do tipo string
+const TEXTO = "Um texto";
+const OUTRO_TEXTO = "Outro texto";
+//TEXTO = 'Mudou o valor da constante';
+let nome;
+nome = "Claudia";
+nome = "Katia";
+console.log("um texto");
---
+++ Typescript/typescript/TiposDeVariaveis.ts
@@ -0,0 +1,16 @@
+//Novos tipos
+
+type NumerOrString = number | string;
+
+//Exemplos de constantes
+//Constantes do tipo string
+const TEXTO: string = "Um texto";
+const OUTRO_TEXTO: string = "Outro texto";
+
+//TEXTO = 'Mudou o valor da constante';
+
+let nome: string;
+
+nome = "Claudia";
+nome = "Katia";
+console.log("um texto");
